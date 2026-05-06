32c44e9bd70f5ef70a772cbf5522424bc882efba
Amirul <amrlls2606@gmail.com>
2026-05-06 18:57:25 +0800

    correct design error

--- index.php
+++ index.php
@@ -316,6 +316,7 @@ $username   = $_SESSION['username'] ?? 'Guest';
             line-height: 1.5;
             display: -webkit-box;
             -webkit-line-clamp: 2;
+            line-clamp: 2;
             -webkit-box-orient: vertical;
             overflow: hidden;
         }
