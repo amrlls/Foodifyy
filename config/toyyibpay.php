c233bc557ae506e64fc43acb047df5bb254dfc6a
Amirul <amrlls2606@gmail.com>
2026-06-03 10:52:21 +0800

    Remove sensitive config files

--- config/toyyibpay.php
+++
@@ -1,6 +0,0 @@
-<?php
-define('TOYYIBPAY_SECRET_KEY',    'wh7fd86b-mqsg-uzhj-uxz9-o4acugk0e9e8');
-define('TOYYIBPAY_CATEGORY_CODE', 'efal4sea');
-define('TOYYIBPAY_BASE_URL',      'https://toyyibpay.com');
-define('TOYYIBPAY_RETURN_URL',    'http://localhost/Foodifyy/modules/shop/payment_return.php');
-define('TOYYIBPAY_CALLBACK_URL',  'http://localhost/Foodifyy/modules/shop/payment_callback.php');
