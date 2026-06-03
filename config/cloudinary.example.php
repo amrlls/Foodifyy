d0cac4d843b26b979324e997c640447fec487e5d
Amirul <amrlls2606@gmail.com>
2026-06-03 11:41:05 +0800

    Add example config files

---
+++ config/cloudinary.example.php
@@ -0,0 +1,4 @@
+<?php
+define('CLOUDINARY_CLOUD_NAME', 'your_cloud_name');
+define('CLOUDINARY_API_KEY',    'your_api_key');
+define('CLOUDINARY_API_SECRET', 'your_api_secret');
---
+++ config/toyyibpay.example.php
@@ -0,0 +1,6 @@
+<?php
+define('TOYYIBPAY_SECRET_KEY',    'your_secret_key_here');
+define('TOYYIBPAY_CATEGORY_CODE', 'your_category_code_here');
+define('TOYYIBPAY_BASE_URL',      'https://toyyibpay.com');
+define('TOYYIBPAY_RETURN_URL',    'http://localhost/Foodifyy/modules/shop/payment_return.php');
+define('TOYYIBPAY_CALLBACK_URL',  'http://localhost/Foodifyy/modules/shop/payment_callback.php');
