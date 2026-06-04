184696eaf52ce76d1edf5e4b7d1e177a761b1e91
Amirul <amrlls2606@gmail.com>
2026-06-04 21:34:41 +0800

    update config template

--- config/cloudinary.example.php
+++ config/cloudinary.example.php
@@ -1,4 +1,15 @@
 <?php
-define('CLOUDINARY_CLOUD_NAME', 'your_cloud_name');
-define('CLOUDINARY_API_KEY',    'your_api_key');
-define('CLOUDINARY_API_SECRET', 'your_api_secret');
+require_once __DIR__ . '/../vendor/autoload.php';
+
+use Cloudinary\Configuration\Configuration;
+
+Configuration::instance([
+    'cloud' => [
+        'cloud_name' => 'your_cloud_name',
+        'api_key'    => 'your_api',
+        'api_secret' => 'your_api_secret',
+    ],
+    'url' => [
+        'secure' => true
+    ]
+]);
