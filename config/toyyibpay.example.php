41da1c8af53f5d0193c4daf42cb421bee0a177e3
Amirul <amrlls2606@gmail.com>
2026-06-12 20:04:08 +0800

    update local host access payment

--- config/toyyibpay.example.php
+++ config/toyyibpay.example.php
@@ -6,5 +6,9 @@ define('TOYYIBPAY_BASE_URL',      'https://toyyibpay.com');
 $protocol = (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off') ? 'https' : 'http';
 $baseUrl  = $protocol . '://' . $_SERVER['HTTP_HOST'];
 
-define('TOYYIBPAY_RETURN_URL',   $baseUrl . '/modules/shop/payment_return.php');
-define('TOYYIBPAY_CALLBACK_URL', $baseUrl . '/modules/shop/payment_callback.php');
+// Auto detect subfolder
+$scriptDir = dirname(dirname(dirname($_SERVER['SCRIPT_NAME']))); 
+$appBase   = rtrim($scriptDir, '/');
+
+define('TOYYIBPAY_RETURN_URL',   $baseUrl . $appBase . '/modules/shop/payment_return.php');
+define('TOYYIBPAY_CALLBACK_URL', $baseUrl . $appBase . '/modules/shop/payment_callback.php');
